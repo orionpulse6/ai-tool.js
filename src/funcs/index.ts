393255592c44b3e9c01ee69df1529fdaca9c010f
orionpulse61 <orionpulse61@gmail.com>
2024-03-11 09:24:40 +0800

    chore: remove exported utils/split-sentence to utils

--- src/funcs/index.ts
+++ src/funcs/index.ts
@@ -1,9 +1,23 @@
 import { ToolFunc } from "../tool-func";
+import { isSimilar } from "./is-similar";
+import { lrucache } from "./lrucache";
+import { mergeSegments } from "./merge-segments";
+import { segments } from "./segments";
 import { similarity } from './similarity';
+import { splitSegments } from "./split-segments";
 
+export * from './lrucache';
 export * from './similarity';
-export * from '../utils/split-sentence';
+export * from './is-similar';
+export * from './split-segments';
+export * from './merge-segments';
+export * from './segments';
 
 export function registerCoreTools() {
+  ToolFunc.register(lrucache);
   ToolFunc.register(similarity);
+  ToolFunc.register(isSimilar);
+  ToolFunc.register(splitSegments);
+  ToolFunc.register(mergeSegments);
+  ToolFunc.register(segments);
 }
