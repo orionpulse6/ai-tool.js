fe916fa4ef883dcf84da59b4908b07d6845401b1
orionpulse61 <orionpulse61@gmail.com>
2024-03-11 09:23:03 +0800

    feat: export utils functions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './base-error'
+export * from './utils'
 export * from './tool-func'
 export * from './funcs'
---
+++ src/utils/index.ts
@@ -0,0 +1,2 @@
+export * from './split-sentence'
+export * from './trunc-to'
