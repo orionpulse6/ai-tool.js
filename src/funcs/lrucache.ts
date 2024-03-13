ff85dff56d1b6375c516652078ea46b0a846e3f8
orionpulse61 <orionpulse61@gmail.com>
2024-03-13 16:48:29 +0800

    fix(lrucache): can pass object options now

--- src/funcs/lrucache.ts
+++ src/funcs/lrucache.ts
@@ -3,31 +3,27 @@ import type { ICacheOptions } from 'secondary-cache'
 import { ToolFunc } from '../tool-func'
 
 
-export function _lrucache(this: ToolFunc, key: string, value?: any, options?: ICacheOptions|number): _Cache {
+export function _lrucache(this: ToolFunc, key: string, value?: any, options?: ICacheOptions | number): _Cache {
   if (key === undefined) {
     if (options) this.cache.setDefaultOptions(options)
   } else if (value === null) {
     return this.cache.del(key)
   } else if (value !== undefined) {
-    if (typeof options === 'number') {
-      return this.cache.set(key, value, options)
-    } else {
-      return this.cache.set(key, value)
-    }
+    return this.cache.set(key, value, options)
   } else {
     return this.cache.get(key)
   }
   return this.cache;
 }
 
-export function createLRUCache(name: string, options?: ICacheOptions|number) {
+export function createLRUCache(name: string, options?: ICacheOptions | number) {
   const result = ToolFunc.get(name) ?? new ToolFunc(name, {
     func: _lrucache,
     description: 'get/set LRU cache or return the LRUCache object',
     params: [
-      {name: 'key', type: 'string', description: 'the key is undefined means change the default cache options'},
-      {name: 'value', type: 'any', description: 'the value to store, if value is null means remove the key'},
-      {name: 'options', type: 'object|number', description: 'the optional cache options:{capacity,expires,cleanInterval} or expires'},
+      { name: 'key', type: 'string', description: 'the key is undefined means change the default cache options' },
+      { name: 'value', type: 'any', description: 'the value to store, if value is null means remove the key' },
+      { name: 'options', type: 'object|number', description: 'the optional cache options:{capacity,expires,cleanInterval} or expires' },
     ],
     result: 'any',
   })
