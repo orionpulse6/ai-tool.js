0abf1a06cfff94875bc6ec3accd55bfa1bded63a
orionpulse61 <orionpulse61@gmail.com>
2024-04-01 20:19:56 +0800

    feat(ClientTools): add stream supports and getUrlParams check empty params now

--- src/client-tools.ts
+++ src/client-tools.ts
@@ -1,6 +1,6 @@
 import { throwError } from "./utils/base-error";
 import { Funcs, ToolFunc } from "./tool-func";
-import { RemoteToolFuncSchema, RemoteFuncItem } from "./utils/consts";
+import { RemoteToolFuncSchema, type RemoteFuncItem, type ActionName } from "./utils/consts";
 
 export interface ClientFuncItem extends RemoteFuncItem {
 }
@@ -46,10 +46,17 @@ export class ClientTools extends ToolFunc {
   }
 
   getUrlParams(objParam: any) {
-    return objParam !== undefined ? '?p=' + encodeURIComponent(JSON.stringify(objParam)) : ''
+    if (objParam !== undefined) {
+      const objParamStr = JSON.stringify(objParam)
+      if (objParamStr !== '{}' && objParamStr !== '[]' && objParamStr !== '""') {
+        // not empty params
+        return '?p=' + encodeURIComponent(objParamStr)
+      }
+    }
+    return ''
   }
 
-  async fetch(objParam?: any) {
+  async fetch(objParam?: any, act?: ActionName, subName?: string) {
     const fetchOptions = {...this.fetchOptions}
     if (!fetchOptions.headers || !fetchOptions.headers['Content-Type']) {
       fetchOptions.headers = {
@@ -57,23 +64,32 @@ export class ClientTools extends ToolFunc {
         ...fetchOptions.headers,
       }
     }
+    if (objParam?.stream && !fetchOptions.headers.Connection) {
+      fetchOptions.headers.Connection = 'keep-alive'
+    }
+    if (!act) { act = this.action!}
+    if (act === 'res') { act = 'get' }
+    subName = subName ? this.name + '/' + subName : this.name
+
+    fetchOptions.method =act.toUpperCase()
     let urlPart: string
-    if (this.action === 'get') {
-      urlPart  = this.name + this.getUrlParams(objParam)
+    if (act === 'get' || act === 'delete') {
+      urlPart  = subName + this.getUrlParams(objParam)
     } else {
-      fetchOptions.method = 'POST'
       fetchOptions.body = JSON.stringify(objParam)
-      urlPart = this.name!
-      // defaults to post
+      urlPart = subName!
     }
 
-    console.log('🚀 ~ ClientTools ~ fetch:', `${this.apiRoot}/${urlPart}`)
+    console.log('🚀 ~ ClientTools ~ fetch:', fetchOptions.method, `${this.apiRoot}/${urlPart}`)
     const res = await fetch(`${this.apiRoot}/${urlPart}`, fetchOptions)
     return res
   }
 
   async func(objParam: any) {
     const res = await this.fetch(objParam)
+    if (objParam?.stream) {
+      return res
+    }
     const result = await res.json()
     return result
   }
