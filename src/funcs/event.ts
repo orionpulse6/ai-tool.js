37f3d42389880713b8f7403153b61fcfe83ffd04
orionpulse61 <orionpulse61@gmail.com>
2024-04-06 16:00:35 +0800

    fix(EventToolFunc): use full EventEmitter

--- src/funcs/event.ts
+++ src/funcs/event.ts
@@ -1,15 +1,17 @@
-import { wrapEventEmitter } from 'events-ex';
+import { EventEmitter } from 'events-ex';
 import { ToolFunc } from '../tool-func';
 import { EventBusName } from '../utils/event';
 
 export class EventToolFunc extends ToolFunc {
-  static _emitter = wrapEventEmitter()
+  // static _emitter = wrapEventEmitter()
+  _emitter = new EventEmitter()// wrapEventEmitter()
 
   description = 'Return event bus'
   result = 'event'
 
   get emitter() {
-    return (this.constructor as unknown as any)._emitter
+    // return (this.constructor as unknown as any)._emitter
+    return this._emitter
   }
 
   func() {
