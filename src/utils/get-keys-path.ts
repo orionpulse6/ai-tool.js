14779aa7645299b0e9fb545f668f0d89ab25b444
orionpulse61 <orionpulse61@gmail.com>
2024-05-14 14:23:50 +0800

    docs: comments added

--- src/utils/get-keys-path.ts
+++ src/utils/get-keys-path.ts
@@ -2,19 +2,49 @@ function isObject(value: any): value is object {
   return !!value && value.constructor === Object
 }
 
+/**
+ * Retrieves an array of all key paths as strings for a nested object or array.
+ * @param value - The object or array to extract the paths from.
+ * @returns - An array of strings containing all the key paths.
+ *
+ * @example
+* ```
+* const obj = { a: { b: { c: 1 } }, d: [0, { e: 2 }] };
+* console.log(getKeysPath(obj)); // Output: ['a.b.c', 'd[0]', 'd[1].e']
+* ```
+*/
 export function getKeysPath<TValue extends object>(value: TValue): string[] {
   if (!value) return []
   return getKeys(value, [], '')
 }
 
-function getKeys(nested: any, paths: string[], dot = '.'): string[] {
-  if (isObject(nested)) {
-    return Object.entries(nested).flatMap(([k, v]) =>
+/**
+ * Retrieves an array of all key paths as strings for a nested object or array.
+ * @param obj - The object or array to extract the paths from.
+ * @param paths - An array representing the current path.
+ * @param dot - The prefix used to join key path segments for object, default is '.'.
+ * @returns - An array of strings containing all the key paths.
+ *
+ * @example
+ * ```
+ * const obj = { a: { b: { c: 1 } }, d: [0, { e: 2 }] };
+ * console.log(getKeys(obj)); // Output: ['a.b.c', 'd[0]', 'd[1].e']
+ * ```
+ */
+function getKeys(obj: any, paths: string[], dot = '.'): string[] {
+  // Perform a depth-first search, traversing each key until the deepest leaf node
+  if (Array.isArray(obj)) {
+    return obj.flatMap((item, i) => getKeys(item, [...paths, `[${i}]`]))
+  }
+
+  if (isObject(obj)) {
+    return Object.entries(obj).flatMap(([k, v]) =>
       getKeys(v, [...paths, dot + k])
     )
   }
-  if (Array.isArray(nested)) {
-    return nested.flatMap((item, i) => getKeys(item, [...paths, `[${i}]`]))
-  }
+
+  // Recursive traversal ends here, indicating the current node is a leaf node
+  // At this point, `paths` contains the complete path to this leaf node's key
+  // Concatenate the path parts and return the result as a string
   return [paths.join('')]
 }
