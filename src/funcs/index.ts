55f9671664a298fa0f38fed6b1f625c35aba6d13
orionpulse61 <orionpulse61@gmail.com>
2024-07-08 08:26:12 +0800

    build: upgrade packages

--- src/funcs/index.ts
+++ src/funcs/index.ts
@@ -1,27 +1,12 @@
 import { event } from "./event";
-import { isSimilar } from "./is-similar";
 import { lrucache } from "./lrucache";
-import { mergeSegments } from "./merge-segments";
-import { segments } from "./segments";
-import { similarity } from './similarity';
-import { splitSegments } from "./split-segments";
 
 export * from './event';
 export * from './lrucache';
-export * from './similarity';
-export * from './is-similar';
-export * from './split-segments';
-export * from './merge-segments';
-export * from './segments';
 export * from './event-client';
 export * from './event-server';
 
 export function registerCoreTools() {
   event.register();
   lrucache.register();
-  similarity.register();
-  isSimilar.register();
-  splitSegments.register();
-  mergeSegments.register();
-  segments.register();
 }
