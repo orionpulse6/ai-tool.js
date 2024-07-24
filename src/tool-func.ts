e2475cab83a0bc23ab2269c32edc9ad836142929
orionpulse61 <orionpulse61@gmail.com>
2024-07-24 20:11:55 +0800

    fix(ToolFunc): const ToolAsyncMultiTas should be ToolAsyncMultiTaskBit

--- src/tool-func.ts
+++ src/tool-func.ts
@@ -4,19 +4,19 @@ import { NotFoundError, throwError } from './utils/base-error';
 import { IntSet } from './utils';
 
 // the binary bit position
-export const ToolAsyncMultiTask = 0
+export const ToolAsyncMultiTaskBit = 0
 export const ToolAsyncCancelableBit = 1
 export const ToolAsyncPriorityBit = 2
 
 export enum AsyncFeatureBits {
-  MultiTask = ToolAsyncMultiTask,
+  MultiTask = ToolAsyncMultiTaskBit,
   Cancelable = ToolAsyncCancelableBit,
   Priority = ToolAsyncPriorityBit,
 }
 
 // bit fields
 export enum AsyncFeatures {
-  MultiTask = 1 << ToolAsyncMultiTask, // B0001
+  MultiTask = 1 << ToolAsyncMultiTaskBit, // B0001
   Cancelable = 1 << ToolAsyncCancelableBit, // B010
   Priority = 1 << ToolAsyncPriorityBit, // B0100
 }
