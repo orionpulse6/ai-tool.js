9260d796cdb43df5320362b947cf6f30232609b1
orionpulse61 <orionpulse61@gmail.com>
2024-08-30 08:51:14 +0800

    feat(util): add memoize

---
+++ src/utils/memoize.ts
@@ -0,0 +1,43 @@
+import _memoize, {type Options} from 'memoizee'
+import { xxhashAsStr } from './hash'
+import { canonicalize } from 'json-canonicalize'
+
+// export interface IMemoizeFnOptions {
+//   // Whether to memoize the function
+//   memoize?: boolean
+//   // Memoizing asynchronous function
+//   // promise?: boolean | "then" | "done" | "done:finally"
+//   // the object instance for method call
+//   this?: any
+// }
+
+export function memoize<F extends (...args: any[]) => any>(fn: any, options?: Options<F>) {
+  if (isAsyncFunction(fn) || isPromise(fn)) {
+    options = {promise: true, ...options}
+  }
+
+  if (options?.length === undefined) {
+    // defaults to Dynamic arguments length
+    options = {length: false, ...options}
+
+  }
+
+  return _memoize<F>(fn, {
+    normalizer: (args) => {
+      return xxhashAsStr(canonicalize(args));
+    },
+    ...options
+  })
+}
+
+function isPromise(value: any) {
+  return (
+    value &&
+    (typeof value === 'object' || typeof value === 'function') &&
+    typeof value.then === 'function'
+  );
+}
+
+function isAsyncFunction(fn: any) {
+  return typeof fn === 'function' && fn.constructor.name === 'AsyncFunction';
+}
