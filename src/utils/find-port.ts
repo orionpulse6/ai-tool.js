0aeffade32c16b49fb19125faea89948ac794e97
orionpulse61 <orionpulse61@gmail.com>
2024-09-03 09:38:56 +0800

    fix(utils): ts declaration

--- src/utils/find-port.ts
+++ src/utils/find-port.ts
@@ -1,17 +1,17 @@
 import net from 'net'
 
-export async function findPort(port, portRetryCount = 10): Promise<number> {
+export async function findPort(port: string|number, portRetryCount = 10): Promise<number> {
   return new Promise((resolve, reject) => {
     if (port === undefined) {
       port = 0
     } else {
-      port = parseInt(port)
-      if (!(port >= 0)) port = 0
+      if (typeof port === 'string') {port = parseInt(port)}
+      if (!(port >= 0)) {port = 0}
     }
     const server = net.createServer();
     server.on('error', (err: any) => {
       if (err.code === 'EADDRINUSE') {
-        port++
+        (port as number)++
         portRetryCount--
         if (portRetryCount > 0) {
           server.listen(port)
